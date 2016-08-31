2d295d9160251f3ff3017091c61d0779206615a8
BBD\james.barrett <james.barrett@brightblueday.com>
2016-08-31 17:24:04 +0100

    jQuery validation begun, to be finished

--- js/functions.js
+++ js/functions.js
@@ -13,6 +13,15 @@ $(document).ready(function(){
 	});
 });
 
-$('html, body').stop().animate({
-     'scrollTop': $target.offset().top
-}, 900, 'swing');
+function checkLength() {
+  if($(this).val().length < 1) {
+    $("fieldset:first").addClass("has-danger");
+  } else {
+    $("fieldset:first").removeClass("has-danger");
+  }
+}
+
+$('#name').keyup(checkLength);
+$('#name').focus(checkLength);
+$('#message').keyup(checkLength);
+$('#message').focus(checkLength);
