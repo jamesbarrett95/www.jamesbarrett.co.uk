85e9df14a7748b8097e2da82f3a15d73db3fa4ee
BBD\james.barrett <james.barrett@brightblueday.com>
2017-08-15 10:45:29 +0100

    Social Media hero amends

--- inc/sections/hero.php
+++ inc/sections/hero.php
@@ -1,5 +1,10 @@
     <div id="top" class="jumbotron jumbotron-fluid no-margin-bottom">
-      <div class="container text-xs-center">
+      <div class="container text-xs-center relative">
+      <ul class="hero-social p-l-0 m-b-0">
+              <li class="m-y-1 large"><a href="https://twitter.com/james_barrett" target="_blank"><i class="fa fa-3x fa-twitter" aria-hidden="true"></i></a></li>
+              <li class="m-y-1 large"><a href="https://github.com/jamesbarrett95" target="_blank"><i class="fa fa-3x fa-github" aria-hidden="true"></i></a></li>
+              <li class="m-y-1 large"><a href="https://www.linkedin.com/in/jamesbarrett95/" target="_blank"><i class="fa fa-3x fa-linkedin" aria-hidden="true"></i></a></li>
+            </ul>
         <noscript>
             <div style="margin-bottom: 40px;">
               <h3 class="white">In order to contact me you must have JavaScript enabled.</h1>
@@ -17,10 +22,5 @@
               <button class="contactBtn m-b-3" type="button" data-toggle="modal" data-target="#contact">Contact me here</button>
               <a href="#about"><i class="fa fa-2x fa-chevron-down" aria-hidden="true"></i></a>
             <?php } ?>
-            <ul class="hero-social p-l-0 m-b-0">
-              <li class="m-y-1 large"><a href="https://twitter.com/james_barrett" target="_blank"><i class="fa fa-3x fa-twitter" aria-hidden="true"></i></a></li>
-              <li class="m-y-1 large"><a href="https://github.com/jamesbarrett95" target="_blank"><i class="fa fa-3x fa-github" aria-hidden="true"></i></a></li>
-              <li class="m-y-1 large"><a href="https://www.linkedin.com/in/jamesbarrett95/" target="_blank"><i class="fa fa-3x fa-linkedin" aria-hidden="true"></i></a></li>
-            </ul>
       </div>
     </div>
